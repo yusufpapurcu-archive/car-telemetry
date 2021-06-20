bb85df3609a15d19a3d64ffbd232ebe90dea99fc
yusufpapurcu <yusufturhanp@gmail.com>
2021-06-20 15:16:46 +0300

    (dev): I'm learning to use prometheus

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yusufpapurcu/car-telemetry
 
 go 1.16
+
+require github.com/prometheus/client_golang v1.11.0
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func recordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
 
 func main() {
-	fmt.Println("Hello World 🏎")
+	recordMetrics()
+
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":2112", nil))
 }
